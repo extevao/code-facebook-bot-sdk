8c5f4236daa6aa79c83c22ecb6733f21ae14fe64
Estevao Vasques <estevaovasques@hotmail.com>
2018-02-09 20:27:41 -0200

    Identado

--- src/SenderRequest.php
+++ src/SenderRequest.php
@@ -2,11 +2,13 @@
 
 namespace CodeBot;
 
-class SenderRequest {
+class SenderRequest
+{
 
     private $event;
 
-    public function __construct() {
+    public function __construct()
+    {
 
         $event = file_get_contents("php://input");
         $event = json_decode($event, true, 512, JSON_BIGINT_AS_STRING);
@@ -14,21 +16,24 @@ class SenderRequest {
 
     }
 
-    public function getSenderId() {
+    public function getSenderId()
+    {
         return $this->event['sender']['id'] ?? null;
     }
 
-    public function getMessage() {
+    public function getMessage()
+    {
         return $this->event['message']['text'] ?? null;
     }
 
-    public function getPostback() {
+    public function getPostback()
+    {
 
-        if(empty($this->event['postback'])){
+        if (empty($this->event['postback'])) {
             return null;
         }
 
-        if(is_array($this->event['postback']) and !empty($this->event['postback']['payload'])) {
+        if (is_array($this->event['postback']) and !empty($this->event['postback']['payload'])) {
             $this->event['postback']['payload'];
         }
         return $this->event['postback'];
