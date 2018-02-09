0000edac4b10633c2386ca99dd6de60f726c6d5c
Estevao Vasques <estevaovasques@hotmail.com>
2018-02-09 20:33:19 -0200

    Identando

--- src/WebHook.php
+++ src/WebHook.php
@@ -8,14 +8,16 @@
 
 namespace CodeBot;
 
-class WebHook {
+class WebHook
+{
 
-    public function check(string $token) {
+    public function check(string $token)
+    {
 
         $hubMode = filter_input(INPUT_GET, 'hub_mode');
         $hubVerifyToken = filter_input(INPUT_GET, 'hub_verify_token');
 
-        if($hubMode == 'subscribe' and $hubVerifyToken == $token){
+        if ($hubMode == 'subscribe' and $hubVerifyToken == $token) {
             return filter_input(INPUT_GET, 'hub_challenge');
         }
 
