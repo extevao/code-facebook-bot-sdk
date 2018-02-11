ad78b52071c2215df3b0338e5ff1bb2e2636dd28
Estevao Vasques <estevaovasques@hotmail.com>
2018-02-11 21:01:53 -0200

    Template de botoes

---
+++ src/Element/ElementInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: estevao
+ * Date: 11/02/18
+ * Time: 15:57
+ */
+
+namespace CodeBot\Element;
+
+interface ElementInterface
+{
+    public function get(): array;
+}
